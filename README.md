1be842840f7d931d0575476030c403bb64e193aa
MaxPuig <45133421+MaxPuig@users.noreply.github.com>
2023-01-14 21:57:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# spotify-reorder-playlist
+# Spotify Reorder Playlist
 Reorder your spotify playlist in ascending/descending order by date added.
+
+## Motivation
+I have a playlist with many songs and I wanted to reorder in ascending order by date added. This new order should be the same for everyone, not just locally. I couldn't find any way to do this in the spotify app so I decided to write a script to do it for me.
+
+## Installation
+### Prerequisites
+- Own the playlist
+- Have node.js and npm installed
+  - You can install them from [here](https://nodejs.org/en/download/)
+- Have your spotify credentials (Client ID and Client Secret)
+  - Go to [Spotify Developer Dashboard](https://developer.spotify.com/dashboard) and create a new app
+  - Copy the Client ID and Client Secret
+  - Go to `Edit Settings` and add `http://localhost:1234/callback` to the Redirect URIs
+  - Save and you're done!
+
+## Steps
+1. Clone the repository: `git clone https://github.com/MaxPuig/spotify-reorder-playlist.git`
+2. Install libraries: `npm install`
+3. Modify `config.json` with your spotify `clientId`, `clientSecret` and `playlistId`
+   > The `playlistId` looks like this `"0BxSFctZ12PYY7ysO9mrTc"` and can be found in the url of the playlist.
+4. Run the script `node index.js`
+5. Access http://localhost:1234/authorize to authorize the app to access your spotify account and start the reordering process
+   > When authorizing it for the first time, it might fail. Just reload the website and try again.
+6. Wait for the script to finish and enjoy your reordered playlist!
---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const config = require('./config.json');
+
+
+const playlistId = config.playlistId;
+const spotifyApi = new SpotifyWebApi({
+    clientId: config.clientId,
+    clientSecret: config.clientSecret,
+    redirectUri: config.redirectUri
+});
+
+
+const app = express();
+
+app.get("/authorize", (req, res) => {
+    const scopes = ["playlist-modify-public", "playlist-modify-private"];
+    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+app.get("/callback", (req, res) => {
+    const { code } = req.query;
+    spotifyApi.authorizationCodeGrant(code)
+        .then(data => {
+            const { access_token, refresh_token } = data.body;
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+            res.redirect("/reorder");
+        })
+        .catch(err => {
+            console.log("Something went wrong!", err);
+            res.redirect("/");
+        });
+});
+
+app.get("/", (req, res) => { res.send("Welcome to your application"); });
+
+app.get("/reorder", async (req, res) => {
+    try {
+        // Get the tracks of the playlist
+        const tracks = await spotifyApi.getPlaylistTracks(playlistId, { offset: 0, limit: 100 });
+        if (tracks.body.total > 100) {
+            for (let i = 0; i < Math.floor(tracks.body.total / 100); i++) {
+                const nextTracks = await spotifyApi.getPlaylistTracks(playlistId, { offset: (i + 1) * 100, limit: 100 });
+                tracks.body.items.push(...nextTracks.body.items);
+            }
+        }
+        let trackList = tracks.body.items;
+        let songs = [];
+        let i = 0;
+        for (let track of trackList) {
+            track.originalPosition = i;
+            i++;
+            songs.push(track);
+        }
+        // Order the tracks by the date they were added, newest to oldest and create a variable for the new position to each track
+        songs.sort((a, b) => new Date(a.added_at) - new Date(b.added_at));
+        songs.reverse();
+        let j = 0;
+        for (let song of songs) {
+            song.newPosition = j;
+            j++;
+        }
+        // Sort the songs by their original position
+        songs.sort((a, b) => new Date(a.originalPosition) - new Date(b.originalPosition));
+        // Reorder the playlist
+        for (let to = 0; to < songs.length; to++) {
+            let from;
+            for (let j = 0; j < songs.length; j++) {
+                if (to == songs[j].newPosition) from = songs[j].originalPosition;
+            }
+            if (to != from) {
+                await sleep(2000); // prevent being ratelimited
+                await spotifyApi.reorderTracksInPlaylist(playlistId, from, to);
+                songs = arraymove(songs, from, to);
+                for (let k = to + 1; k < from + 1; k++) {
+                    songs[k].originalPosition++;
+                }
+                console.log("Moved from: " + from + " to: " + to);
+            } else {
+                console.log("Skipped: " + to + " - " + from);
+            }
+        }
+        res.status(200).send("Playlist reordered successfully");
+    } catch (err) {
+        console.log("Something went wrong", err);
+        res.status(500).send("An error occurred while trying to reorder the playlist");
+    }
+});
+
+
+app.listen(1234, () => {
+    console.log("Server running at http://localhost:1234/authorize");
+});
+
+
+function arraymove(arr, fromIndex, toIndex) {
+    var element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+    return arr;
+}
+
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
